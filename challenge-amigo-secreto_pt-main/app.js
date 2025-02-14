19c86d3b7aeeadf448a976d83f00837126cc142d
Anderson Sales <andersonoar@gmail.com>
2025-02-14 17:58:56 -0300

    criando função para adcionar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,4 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//armazenar os nomes digitados
 let amigos = [];
+
+function adcionarAmigos ();
