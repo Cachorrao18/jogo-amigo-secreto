971b0415a1c62f3153034bb3d30109ac6f81ee99
Anderson Sales <andersonoar@gmail.com>
2025-02-14 18:20:04 -0300

    Impedindo nomes repetidos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -4,4 +4,13 @@ let amigos = [];
 // armazenar os nomes digitados
 function adcionarAmigos () {
     let amigo = document.getElementById('amigo').value.trim();
+// verificar se tem algum valor no campo
+    if (amigo === '') {
+        alert('Insira um nome aqui')
+        return
+    }
+    if (amigos.includes(amigo)) {
+        alert('Esse nome já foi adcionado')
+    }
+
 }
